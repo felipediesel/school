f7c835b990bca3f8280760adbaee0e9315a4cf99
Felipe Diesel <felipediesel@gmail.com>
2016-03-23 19:07:36 -0300

    Updating config files for Heroku.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module School
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.app = config_for(:app)
   end
 end

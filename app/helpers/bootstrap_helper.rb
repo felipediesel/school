fdde532a731ee6a144c8e4add8dea4f00294ee5c
Felipe Diesel <felipediesel@gmail.com>
2016-06-09 07:54:56 -0300

    Adds a helper for modal_header, refactor close_button and adds tests for bootstrap helper.

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -11,8 +11,27 @@ module BootstrapHelper
     content_tag :div, body, class: "alert alert-#{key}", role:"alert"
   end
 
-  def close_button
-    '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.html_safe
+  def modal_header(title_content = nil, options = {})
+    title_content ||= title
+    options.reverse_merge! close: true
+
+    content_tag :div, class: 'modal-header' do
+      html = []
+      html << modal_close_button if options.delete(:close)
+      html << content_tag(:h4, title_content, class: 'modal-title')
+
+      safe_join html
+    end
+  end
+
+  def modal_close_button
+    close_button 'modal'
+  end
+
+  def close_button(dismiss = 'alert')
+    button_tag type: 'button', class: 'close', 'data-dismiss' => dismiss, 'aria-label' => t('text.close') do
+      content_tag :span, '&times;'.html_safe, 'aria-hidden' => "true"
+    end
   end
 
   def dropdown_menu(text, &block)
@@ -25,7 +44,7 @@ module BootstrapHelper
     content_tag :div, class: 'btn-group' do
       button_tag(text, button_options) +
       content_tag(:div, class: 'dropdown-menu') do
-        capture &block
+        capture &block if block_given?
       end
     end
   end
---
+++ test/helpers/bootstrap_helper_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class BootstrapHelperTest < ActionView::TestCase
+  TEMPLATE = {
+    alert: %(<div class="alert alert-info" role="alert">Content</div>),
+    translated_alert: %(<div class="alert alert-danger" role="alert">Content</div>),
+    close_button: %(<button name="button" type="button" class="close" data-dismiss="alert" aria-label="Fechar"><span aria-hidden="true">×</span></button>),
+    modal_close_button: %(<button name="button" type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">×</span></button>),
+    dropdown_menu_empty: %(<div class="btn-group"><button name="button" type="button" class="btn btn-secondary btn-sm dropdown-toggle" aria-expanded="false" aria-haspopup="true" data-toggle="dropdown">Text</button><div class="dropdown-menu"></div></div>),
+    dropdown_menu: %(<div class="btn-group"><button name="button" type="button" class="btn btn-secondary btn-sm dropdown-toggle" aria-expanded="false" aria-haspopup="true" data-toggle="dropdown">Text</button><div class="dropdown-menu"><a href="#" class="dropdown-item">Item 1</a><a href="#" class="dropdown-item">Item 2</a></div></div>)
+  }
+  TEMPLATE[:modal_header] = %(<div class="modal-header">#{TEMPLATE[:modal_close_button]}<h4 class="modal-title">Title</h4></div>)
+
+  test "should return the alert" do
+    actual = alert(:info, 'Content')
+    expected = TEMPLATE[:alert]
+    assert_dom_equal expected, actual
+  end
+
+  test "should return the alert translating rails default to bootstrap " do
+    actual = alert(:alert, 'Content')
+    expected = TEMPLATE[:translated_alert]
+    assert_dom_equal expected, actual
+  end
+
+  test "should return the close button" do
+    actual = close_button
+    expected = TEMPLATE[:close_button]
+    assert_dom_equal expected, actual
+  end
+
+  test "should return the close button for modal" do
+    actual = modal_close_button
+    expected = TEMPLATE[:modal_close_button]
+    assert_dom_equal expected, actual
+  end
+
+  test "should return the modal header " do
+    actual = modal_header 'Title'
+    expected = TEMPLATE[:modal_header]
+    assert_dom_equal expected, actual
+  end
+
+  test "should return the dropdown empty" do
+    actual = dropdown_menu('Text')
+    expected = TEMPLATE[:dropdown_menu_empty]
+    assert_dom_equal expected, actual
+  end
+
+  test "should return the dropdown with links" do
+    actual = dropdown_menu('Text') do
+      link_to('Item 1', '#', class: 'dropdown-item') +
+      link_to('Item 2', '#', class: 'dropdown-item')
+    end
+    expected = TEMPLATE[:dropdown_menu]
+    assert_dom_equal expected, actual
+  end
+end
